777ef206d3bb4604b7d5c2f26947c75d6184d6e2
dev-quoc <quocduby@gmail.com>
2018-05-12 19:47:32 +0700

    adding files

---
+++ webpack.config.production.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+    // Webpack configuration goes here
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'static/[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+
+            // First Rule
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                importLoaders: 1,
+                                camelCase: true,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                config: {
+                                    ctx: {
+                                        autoprefixer: {
+                                            browsers: 'last 2 versions'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new ExtractTextPlugin({
+            filename: 'styles/styles.[hash].css',
+            allChunks: true
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true,
+        hot: true
+    }
+};
